5d3e160b5a8654381cfa51dd0846f917cce2af75
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2025-02-17 21:46:52 -0400

    feat: made settings page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(settings)" options={{ headerShown: false }} />
         <Stack.Screen name="(price-suggestion)" options={{ 
           title: "",
           headerBackTitle: "Sell Page"
