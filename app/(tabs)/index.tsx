b1f2bb7fb3b5ec42c5e19cb3bf4413071c33d7bb
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2025-02-21 19:10:52 -0400

    feat: added other user listings to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { View, TextInput, ScrollView, SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import { View, TextInput, SafeAreaView, Text, TouchableOpacity, FlatList} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import tw from 'twrnc';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { LoggedInUser } from '@/types/userSchema';
 import { FeedCard } from '@/components/FeedCard';
 import { useRouter } from 'expo-router';
+import { get, off, query, ref} from 'firebase/database';
+import { database } from '@/config/firebaseConfig';
+import { demoListings, Listing } from '@/data/demoListings';
 
 
 export default function HomeScreen() {
   const router = useRouter();
   const [user, setUser] = useState<LoggedInUser | null>(null);
+
+
+  const [listings, setListings] = useState<Listing[]>(demoListings)
+
+  useEffect(() => {
+    const getUserListings = async () => {
+      try {
+        if(user) {
+          console.log(user.uid)
+        const listingsRef = ref(database,'listings')
+  
+        const queryRef = query(listingsRef)
+
+        // Clears cached data
+        off(queryRef)
+        const snapshot = await get(queryRef)
+        if (snapshot.exists()) {
+          const data = snapshot.val() as Record<string, Listing>;
+          let listingData : Listing[] = Object.values(data)
+          
+          listingData = listingData.filter((listing) => {
+            console.log(listing)
+              return listing.seller?.uid !== user.uid
+          })
+          console.log(listingData.length)
+          setListings([...listings,...listingData])
+        }}}
+      catch (err) {
+      console.error('Error fetching data:', err);
+    }}
+    getUserListings()
+  },[user])
 
   useEffect(() => {
     const loadUser = async () => {
@@ -47,10 +82,22 @@ export default function HomeScreen() {
       </View>
 
       {/* Feed Content */}
-      <ScrollView style={tw`flex-1`}>
-        <FeedCard />
-        <FeedCard />
-      </ScrollView>
+      {listings ? 
+        <FlatList
+        data={listings}
+        keyExtractor={(item) => item.lid}
+        renderItem={({ item }) => (
+          <FeedCard
+            listingId={item.lid}
+            title={item.title}
+            price={item.price}
+            image={item.imageUrl}
+            seller={item.seller}
+            description={item.description}
+          />
+        )}
+      />
+      : <></>}
     </SafeAreaView>
   );
 }
