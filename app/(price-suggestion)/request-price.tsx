ef6adac4ee4de440b934ea9438351016afaab68b
GHuyHuynh <huy.huynh@dal.ca>
2025-03-01 12:48:24 -0400

    add keyboard avoid scrool view to sigup page

--- app/(price-suggestion)/request-price.tsx
+++ app/(price-suggestion)/request-price.tsx
@@ -30,14 +30,14 @@ export default function RequestPriceScreen() {
       params: { paramsData: JSON.stringify(data) },
     });
   };
- 
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : undefined}
       keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
       style={tw`flex-1`}
     >
-      <ScrollView 
+      <ScrollView
         ref={scrollViewRef}
         style={tw`bg-neutral-100`}
         contentContainerStyle={tw`flex-grow`}
